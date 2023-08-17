8ea757512257f8ccc1e1fa30d5117ef5545dc9a5
ggyu0629 <ksg1885@naver.com>
2023-08-17 14:38:42 +0900

    project init

---
+++ src/main/java/com/construction/constructionapi/ConstructionApiApplication.java
@@ -0,0 +1,13 @@
+package com.construction.constructionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/construction/constructionapi/ConstructionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construction.constructionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
