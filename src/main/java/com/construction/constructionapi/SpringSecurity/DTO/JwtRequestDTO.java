c9f2e81f34e22f0e62e2ca7c176383aa4b5e939d
ggyu0629 <ksg1885@naver.com>
2023-08-21 02:59:44 +0900

    frontend error test

--- src/main/java/com/construction/constructionapi/SpringSecurity/DTO/JwtRequestDTO.java
+++ src/main/java/com/construction/constructionapi/SpringSecurity/DTO/JwtRequestDTO.java
@@ -1,10 +1,12 @@
 package com.construction.constructionapi.SpringSecurity.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class JwtRequestDTO {
 
     private String email;
