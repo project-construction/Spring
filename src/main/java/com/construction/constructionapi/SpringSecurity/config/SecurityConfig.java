3ff00f1b13c90f8709a3607bb4ba00f699112e35
ggyu0629 <ksg1885@naver.com>
2023-08-24 16:08:22 +0900

    update cors

--- src/main/java/com/construction/constructionapi/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/construction/constructionapi/SpringSecurity/config/SecurityConfig.java
@@ -24,8 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/file/**",
             "/image/**",
             "/swagger/**",
-            "/swagger-ui/**"
+            "/swagger-ui/**",
             // other public endpoints of your API may be appended to this array
+            "/h2/**"
     };
 
     @Bean
