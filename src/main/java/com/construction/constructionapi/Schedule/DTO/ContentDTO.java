6bdbe72a7a741a32627bdd979d46c6a79c9cd0ba
abbiddo@naver.com <abbiddo@naver.com>
2023-09-04 20:04:18 +0900

    DTO update

--- src/main/java/com/construction/constructionapi/Schedule/DTO/ContentDTO.java
+++ src/main/java/com/construction/constructionapi/Schedule/DTO/ContentDTO.java
@@ -3,10 +3,13 @@ package com.construction.constructionapi.Schedule.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ContentDTO {
 
     private String time;
+    private LocalDateTime dateTime;
     private String content;
 }
