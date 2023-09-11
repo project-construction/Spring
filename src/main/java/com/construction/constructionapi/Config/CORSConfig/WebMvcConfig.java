056389333834b24ad85782e0cfce372f529c92f2
abbiddo@naver.com <abbiddo@naver.com>
2023-09-11 15:07:42 +0900

    update empty delete

--- src/main/java/com/construction/constructionapi/Config/CORSConfig/WebMvcConfig.java
+++ src/main/java/com/construction/constructionapi/Config/CORSConfig/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("https://web-template-3prof2llkxuyz4l.sel4.cloudtype.app", "http://localhost:3000")
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "DELETE");
     }
 
 }
